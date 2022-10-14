328e80e3f31e3c3c670b3e7b9d4cdd662f7ec1de
ROllerozxa <rollerozxa@voxelmanip.se>
2022-10-14 23:14:55 +0200

    Replace deprecated default.player calls with player_api

--- init.lua
+++ init.lua
@@ -1,8 +1,9 @@
+
 minetest.register_globalstep(function(dtime)
 	local players = minetest.get_connected_players()
 	for i=1, #players do
 		local name = players[i]:get_player_name()
-		if default.player_attached[name] and not players[i]:get_attach() and
+		if player_api.player_attached[name] and not players[i]:get_attach() and
 				(players[i]:get_player_control().up == true or
 				players[i]:get_player_control().down == true or
 				players[i]:get_player_control().left == true or
@@ -10,8 +11,8 @@ minetest.register_globalstep(function(dtime)
 				players[i]:get_player_control().jump == true) then
 			players[i]:set_eye_offset({x=0, y=0, z=0}, {x=0, y=0, z=0})
 			players[i]:set_physics_override(1, 1, 1)
-			default.player_attached[name] = false
-			default.player_set_animation(players[i], "stand", 30)
+			player_api.player_attached[name] = false
+			player_api.set_animation(players[i], "stand", 30)
 		end
 	end
 end)
@@ -20,16 +21,16 @@ minetest.register_chatcommand("sit", {
 	description = "Sit down",
 	func = function(name)
 		local player = minetest.get_player_by_name(name)
-		if default.player_attached[name] then
+		if player_api.player_attached[name] then
 			player:set_eye_offset({x=0, y=0, z=0}, {x=0, y=0, z=0})
 			player:set_physics_override(1, 1, 1)
-			default.player_attached[name] = false
-			default.player_set_animation(player, "stand", 30)
+			player_api.player_attached[name] = false
+			player_api.set_animation(player, "stand", 30)
 		else
 			player:set_eye_offset({x=0, y=-7, z=2}, {x=0, y=0, z=0})
-			player:set_physics_override(0, 0, 0)
-			default.player_attached[name] = true
-			default.player_set_animation(player, "sit", 30)
+			player:set_physics_override({speed = 0, jump = 0, gravity = 0})
+			player_api.player_attached[name] = true
+			player_api.set_animation(player, "sit", 30)
 		end
 	end
 })
@@ -38,16 +39,16 @@ minetest.register_chatcommand("lay", {
 	description = "Lay down",
 	func = function(name)
 		local player = minetest.get_player_by_name(name)
-		if default.player_attached[name] then
+		if player_api.player_attached[name] then
 			player:set_eye_offset({x=0, y=0, z=0}, {x=0, y=0, z=0})
 			player:set_physics_override(1, 1, 1)
-			default.player_attached[name] = false
-			default.player_set_animation(player, "stand", 30)
+			player_api.player_attached[name] = false
+			player_api.set_animation(player, "stand", 30)
 		else
 			player:set_eye_offset({x=0, y=-13, z=0}, {x=0, y=0, z=0})
-			player:set_physics_override(0, 0, 0)
-			default.player_attached[name] = true
-			default.player_set_animation(player, "lay", 0)
+			player:set_physics_override({speed = 0, jump = 0, gravity = 0})
+			player_api.player_attached[name] = true
+			player_api.set_animation(player, "lay", 0)
 		end
 	end
 })
